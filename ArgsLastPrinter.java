ad981323cab2c0f116723098f2e7b7212d45d627
yusuke <sugi.ukyo1225@gmail.com>
2023-04-23 17:06:27 +0900

    rgsLastPrinter.java課題提出 #1

---
+++ ArgsLastPrinter.java
@@ -0,0 +1,12 @@
+public class ArgsLastPrinter{
+  /**
+ * @param args
+ */
+public static void main(String[] args){
+    if (args.length ==0){
+        System.out.println("なし");
+    }else {
+         System.out.println("[args.n - 1]");
+    }
+  }
+}
